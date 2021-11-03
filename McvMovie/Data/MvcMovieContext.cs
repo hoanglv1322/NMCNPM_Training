948dc312e5b9d446322dffece6024698f7a4de2a
hoanglv <hoang.lv1322@gmail.com>
2021-11-03 18:48:20 +0700

    tranning

---
+++ McvMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using MvcMovie.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
